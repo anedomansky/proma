13320dde65cd86d368d1960c2600ebb43331370f
anedomansky <anedomansky@googlemail.com>
2020-09-13 11:09:40 +0200

    Worked on the header.

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import logoImg from '../../assets/images/logo.svg';
+import userIcon from '../../assets/icons/user.svg';
 import './Header.scss';
 
 const Header: React.FC = () => (
-    <header>HEADER</header>
+    <header>
+        <img className="logo" src={logoImg} alt="Logo" />
+        <h1 className="title">Fancy IT Company Name</h1>
+        <img className="user-menu" src={userIcon} alt="User-menu" />
+    </header>
 );
 
 export default Header;
