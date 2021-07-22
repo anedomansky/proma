1378ff1345251ca7b335603619a364484270554e
anedomansky <anedomansky@icloud.com>
2021-07-22 19:37:18 +0200

    Worked on the ProjectCreate component.

--- frontend/src/components/heading/Heading.tsx
+++ frontend/src/components/heading/Heading.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './Heading.scss';
 
 interface Props {
-    title: string;
+    title?: string;
 }
 
-const Heading: React.FC<Props> = ({ title }) => (
-    <>
-        <h2>{title}</h2>
-        <hr />
-    </>
-);
+const Heading: React.FC<Props> = ({ title, children }) => {
+    const hasChild = () => Boolean(children);
+
+    if (hasChild()) {
+        return (
+            <>
+                <h2>
+                    {children}
+                </h2>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <h2>{title}</h2>
+            <hr />
+        </>
+    );
+};
 
 export default Heading;
--- frontend/src/components/project-create/ProjectCreate.tsx
+++ frontend/src/components/project-create/ProjectCreate.tsx
@@ -2,19 +2,46 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import backArrow from '../../assets/icons/back-arrow.svg';
 import Button from '../button/Button';
+import Form from '../form/Form';
+import Heading from '../heading/Heading';
+import Input from '../input/Input';
 import './ProjectCreate.scss';
 
 const ProjectCreate: React.FC = () => {
     const history = useHistory();
 
     return (
-        <section>
-            <h1>
+        <section className="project-create">
+            <Heading>
                 <Button type="button" onClick={() => history.goBack()}>
                     <img src={backArrow} alt="Back" />
                 </Button>
                 Create a new project
-            </h1>
+            </Heading>
+            <Form>
+                <Input
+                    label="Name"
+                    id="name"
+                    type="text"
+                    pattern="[0-9A-Za-z ]{2,}"
+                    minLength={2}
+                    value=""
+                    onChange={(validation) => null}
+                />
+                <Input
+                    label="Assign users"
+                    id="users"
+                    type="text"
+                    pattern="[0-9A-Za-z ]{2,}"
+                    minLength={2}
+                    value=""
+                    onChange={(validation) => null}
+                />
+                <span>USERS LIST</span>
+                <div className="submit">
+                    SUBMIT
+                </div>
+            </Form>
         </section>
     );
 };
